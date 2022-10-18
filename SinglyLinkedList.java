3f089063de552324496a435287ad891015ee14dd
deanone <athanasios.salamanis@gmail.com>
2022-10-18 14:27:20 +0300

    Added docstring.

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,28 +1,60 @@
+/**
+ * @author A. Salamanis (athanasios.salamanis@gmail.com)
+ * @version 0.1
+ * @since 2019-06-20
+ */
+
+/**
+ * Node class: A class representing a node of the singly linked list. 
+ */
 class Node {
     int val;
     Node next;
     
+    /**
+     * Constructor. 
+     */
     Node() {
         val = 0;
         next = null;
     }
     
+    /**
+     * Constructor.
+     * 
+     * @param val the initial value of the node
+     */
     Node(int val) {
         this.val = val;
         next = null;
     }
 }
 
+/**
+ * SinglyLinkedList class: A class representing a singly linked list. 
+ */
 class SinglyLinkedList {
-    
+    /**
+     * the head of the singly linked list 
+     */
     Node head;
+
+    /**
+     * the length of the list, i.e., the number of nodes in the list
+     */
     int length;
     
+    /**
+     * Constructor. 
+     */
     public SinglyLinkedList() {
         head = null;
         length = 0;
     }
     
+    /**
+     * Prints the values of the list. 
+     */
     void print() {
         Node current = head;
         while (current != null) {
@@ -32,11 +64,22 @@ class SinglyLinkedList {
         System.out.println("NULL");
     }
     
+    /**
+     * Returns the length of the list.
+     * 
+     * @return the length of the list 
+     */
     int getLength() {
         return length;
     }
     
-    // O(n) time complexity
+    /**
+     * Returns the value of the index_th node of the list.
+     * O(n) time complexity
+     * 
+     * @param index the index of the node whose value will be returned
+     * @return the value of the index_th node of the list 
+     */
     public int get(int index) {
         if (head == null) { // list is empty
             return -1;
@@ -61,7 +104,12 @@ class SinglyLinkedList {
         return current.val;
     }
     
-    // O(1) time complexity
+    /**
+     * Adds a value at the beginning of the list.
+     * O(1) time complexity
+     * 
+     * @param val the value to be added 
+     */
     public void addAtHead(int val) {
         if (head == null) {
             Node node = new Node(val);
@@ -74,7 +122,12 @@ class SinglyLinkedList {
         length++;
     }
     
-    // O(n) time complexity
+    /**
+     * Adds a value at the end of the list.
+     * O(n) time complexity
+     * 
+     * @param val the value to be added 
+     */
     public void addAtTail(int val) {
         if (head == null) { // list is empty
             Node node = new Node(val);
@@ -90,7 +143,13 @@ class SinglyLinkedList {
         length++;
     }
     
-    // O(n) time complexity
+    /**
+     * Adds a value before the index_th node of the list. 
+     * O(n) time complexity
+     * 
+     * @param index the index of the node before of which the new value will be added
+     * @param val the value to be added
+     */
     public void addAtIndex(int index, int val) {
         if (index < 0) {
             return;
@@ -123,7 +182,12 @@ class SinglyLinkedList {
         }
     }
     
-    // O(n) time complexity
+    /**
+     * Deletes the index_th node.
+     * O(n) time complexity
+     * 
+     * @param the index of the node to be deleted
+     */
     public void deleteAtIndex(int index) {
         if (index < 0) {
             return;
